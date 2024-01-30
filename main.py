aec1462b8b03564e19769b22f576d3843301cc62
H@sanjon <99160771+hasanj0n@users.noreply.github.com>
2024-01-30 20:42:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import streamlit as st
+import streamlit as st
 from page1 import loan_prediction
 from page2 import chatBot
+from language import english_translator,uzbek_translator,russian_translator
 
 def main():
-    st.sidebar.title("Navigation")
-    pages = ["Bank Loan Prediction","Chatbot"]
-    selection = st.sidebar.radio("Go to", pages)
-    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-    if selection == "Bank Loan Prediction":
-        loan_prediction()
-    else: 
-        chatBot()
-
+    st.sidebar.title('Select Language/Tilni Tanlang')
+    lang_select=st.sidebar.radio('Languages',['en','ru','uz'])
+    def choose_project(language):
+        st.sidebar.title(language['sidebar title'])
+        pages = [language['page_1'],language['page_2']]
+        selection = st.sidebar.radio(language['sidebar ratio'], pages)
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+        if selection==pages[0]:
+            lang=english_translator()
+            if lang_select=='ru': lang=russian_translator()
+            elif lang_select=='uz': lang=uzbek_translator()
+            loan_prediction(lang)
+        else:
+            lang=english_translator()
+            if lang_select=='ru': lang=russian_translator()
+            elif lang_select=='uz': lang=uzbek_translator() 
+            chatBot()
+    language=english_translator()        
+    if lang_select=='ru':
+        language=russian_translator()        
+    elif lang_select=='uz':
+        language=uzbek_translator()
+        
+    choose_project(language)
 if __name__ == "__main__":
     main()
